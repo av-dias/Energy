8607e0706ba20b04bc716330d8dc51df524d994c
av-dias <al.vrdias@gmail.com>
2025-10-26 15:37:46 +0000

    [Api] Request API logic

---
+++ requests/report.ts
@@ -0,0 +1,43 @@
+import { fetchWithTimeout } from "@/service/serviceUtils";
+
+export const getMonthlyReport = async (
+  user: { uuid: any },
+  month: number,
+  year: number,
+  server: string
+) => {
+  return await fetchWithTimeout(`http://${server}:8080/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+                    {
+                      getMonthlyReport(
+                        userId: "${user?.uuid}"
+                        month: ${month + 1}
+                        year: ${year}
+                      ) {
+                        id
+                        month
+                        totalKwh
+                        totalCost
+                        averageCost
+                        numberOfDays
+                        predictedTotalCost
+                        totalDayCost
+                        totalPeakCost
+                        totalNightCost
+                        totalDayKwh
+                        totalPeakKwh
+                        totalNightKwh
+                        ascFee
+                        psoFee
+                        createdAt
+                      }
+                    }
+            `,
+    }),
+  });
+};
